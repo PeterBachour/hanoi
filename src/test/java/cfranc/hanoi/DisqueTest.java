798299f2faeee1eccf618c0a7fcbbfe334d9e61c
= <peter_bachour@icloud.com>
2020-12-09 16:50:22 +0100

    UnitTests

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -12,18 +12,29 @@ public class DisqueTest {
 
 	@Test
 	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+		assertEquals(-1, dSmall.compareTo(dMedium));
+		assertEquals(1, dTall.compareTo(dSmall));
+		assertEquals(0, dMedium.compareTo(dMedium));
 	}
 
 	@Test
 	public void compareTo_SmallMedium_Negative(){
 		int expected = -1;
 		int actual = dSmall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void compareTo_TallSmall_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void compareTo_TallTall_Zero(){
+		int expected = 0;
+		int actual = dTall.compareTo(dTall);
 		assertEquals(expected, actual);
 	}
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -4,11 +4,50 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.security.cert.TrustAnchor;
+
 public class TourTest {
 
-	@Test
-	public void test() {
+	Tour tourNonVide = new Tour(2);
+	Disque small = new Disque(1);
+	Disque medium = new Disque(2);
+	Disque tall = new Disque(3);
 
+	boolean expected_False = false;
+	boolean isExpected_True = true;
+
+
+	@Test
+	public void testEmpiler_TourNonVideEtTestPlusPetit_True() {
+		tourNonVide.disques.add(medium);
+
+		Boolean actual = tourNonVide.empiler(small);
+		assertEquals(isExpected_True, actual);
 	}
+
+	@Test
+	public void testEmpiler_TourNonVideEtTestPlusGrand_False() {
+		tourNonVide.disques.add(small);
+
+		Boolean actual = tourNonVide.empiler(tall);
+		assertEquals(expected_False, actual);
+	}
+
+	@Test
+	public void testEmpiler_TourNonVideEtTestTourPleine_False() {
+		tourNonVide.disques.add(tall);
+		tourNonVide.disques.add(medium);
+
+		Boolean actual = tourNonVide.empiler(small);
+		assertEquals(expected_False, actual);
+	}
+
+	@Test
+	public void testEmpiler_TourVide_True() {
+		Boolean actual = tourNonVide.empiler(tall);
+		assertEquals(isExpected_True, actual);
+	}
+
+
 
 }
